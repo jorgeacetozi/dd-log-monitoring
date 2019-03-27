3469caff95e5a916518d8f4f67dd9fbe937690e7
Jorge Acetozi <jorge.acetozi@gmail.com>
2019-03-27 19:09:04 -0300

    Create task that will execute a list of alerts periodically

---
+++ src/main/java/com/jorgeacetozi/dd/logmonitoring/tasks/TriggerAlertTask.java
@@ -0,0 +1,27 @@
+package com.jorgeacetozi.dd.logmonitoring.tasks;
+
+import java.util.List;
+import com.jorgeacetozi.dd.logmonitoring.alert.Alert;
+
+/*
+ * This task iterates over a list of alerts and execute each of them, which means two things:
+ * 1) If the alert threshold is crossed, a notification will be sent out using the alert's handlers
+ * 2) If the alert was active and its value is now below the threshold, a recovery notification is sent
+ * out using the alert's handlers
+ */
+public class TriggerAlertTask implements Runnable {
+
+  private final List<Alert> alerts;
+
+  public TriggerAlertTask(List<Alert> alerts) {
+    this.alerts = alerts;
+  }
+
+  @Override
+  public void run() {
+    for (Alert alert : alerts) {
+      alert.execute();
+    }
+  }
+
+}
